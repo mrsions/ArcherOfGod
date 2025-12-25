0e5d2494e8d1c4f1f19d484dc54365b0c18d108e
Sions <mrsions@gmail.com>
2025-12-26 00:24:17 +0900

    버그 수정

--- Assets/AOG/Scripts/Runtime/ArcherOfGod/Components/GameStatusEvent.cs
+++ Assets/AOG/Scripts/Runtime/ArcherOfGod/Components/GameStatusEvent.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 namespace AOT
 {
--- Assets/AOG/Scripts/Runtime/ArcherOfGod/GameManager.cs
+++ Assets/AOG/Scripts/Runtime/ArcherOfGod/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace AOT
 {
